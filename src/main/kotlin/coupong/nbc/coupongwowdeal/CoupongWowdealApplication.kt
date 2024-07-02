010e52b841f1e0a3df3b42c5c6bea5dc914b5c1b
LSH <fhzktm@naver.com>
2024-07-02 16:59:07 +0900

    init project

---
+++ src/main/kotlin/coupong/nbc/coupongwowdeal/CoupongWowdealApplication.kt
@@ -0,0 +1,11 @@
+package coupong.nbc.coupongwowdeal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoupongWowdealApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoupongWowdealApplication>(*args)
+}
---
+++ src/test/kotlin/coupong/nbc/coupongwowdeal/CoupongWowdealApplicationTests.kt
@@ -0,0 +1,13 @@
+package coupong.nbc.coupongwowdeal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoupongWowdealApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
