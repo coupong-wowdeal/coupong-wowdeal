8214cddd9f9883ad31762213dfc0a44ed2ff9bd7
Jaewon Noh <opqw987@gmail.com>
2024-07-03 15:34:25 +0900

    :sparkles: 회원가입 유효성 검증 추가

--- src/main/kotlin/coupong/nbc/coupongwowdeal/domain/user/dto/v1/request/SignUpRequest.kt
+++ src/main/kotlin/coupong/nbc/coupongwowdeal/domain/user/dto/v1/request/SignUpRequest.kt
@@ -8,6 +8,18 @@ data class SignUpRequest(
     val password: String,
     val confirmPassword: String
 ) {
+    init {
+        validate()
+    }
+
+    private fun validate() {
+        require(username.length > 3) { "Username must be longer than 3" }
+        require(username.matches(Regex("^[a-zA-Z0-9]*$"))) { "Username only alphabet and number allowed" }
+        require(password.length > 4) { "Password must be longer than 4" }
+        require(!password.contains(username)) { "Password must not contain username" }
+        require(password == confirmPassword) { "Password must be equal to confirmPassword" }
+    }
+
     fun toEntity(passwordEncoder: PasswordEncoder): User {
         return User(
             username = username,
