ddf5898f628d0e65691393330aef152cf691c83b
Jaewon Noh <opqw987@gmail.com>
2024-07-04 15:54:01 +0900

    :bug: 작동 안하는 FK 어노테이션 삭제

--- src/main/kotlin/coupong/nbc/coupongwowdeal/domain/timedeal/model/v1/TimeDeal.kt
+++ src/main/kotlin/coupong/nbc/coupongwowdeal/domain/timedeal/model/v1/TimeDeal.kt
@@ -3,11 +3,9 @@ package coupong.nbc.coupongwowdeal.domain.timedeal.model.v1
 import coupong.nbc.coupongwowdeal.domain.common.BaseAuditingEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
 import jakarta.persistence.Table
 import java.time.LocalDateTime
 
@@ -21,7 +19,6 @@ class TimeDeal(
 
     var closedAt: LocalDateTime,
 
-    @JoinColumn(name = "coupon_id", foreignKey = ForeignKey(name = "fk_coupon_time_deal"))
     var couponId: Long
 ) : BaseAuditingEntity() {
     @Id
