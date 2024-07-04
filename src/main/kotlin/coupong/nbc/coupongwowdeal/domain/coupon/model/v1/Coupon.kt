33769d42f1429499dc1f4740d914d64dfaf29929
itssu <ssujinn96@gmail.com>
2024-07-04 15:06:35 +0900

    🏷️ expirationAt 컬럼 Couponuser -> Coupon 엔티티로 이동
    Co-authored-by: npureaun <kjihun89@gmail.com>

--- src/main/kotlin/coupong/nbc/coupongwowdeal/domain/coupon/model/v1/Coupon.kt
+++ src/main/kotlin/coupong/nbc/coupongwowdeal/domain/coupon/model/v1/Coupon.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import java.time.LocalDateTime
 
 @Entity
 class Coupon(
@@ -19,6 +20,9 @@ class Coupon(
 
     @Column
     var currentQuantity: Int,
+
+    @Column
+    val expirationAt: LocalDateTime,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
