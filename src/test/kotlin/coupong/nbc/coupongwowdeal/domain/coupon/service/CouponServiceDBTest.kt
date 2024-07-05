7c8ea52e90cb46c9b8d517cfcc3ee1310ee5cd8d
seungheyon <71931476+seungheyon@users.noreply.github.com>
2024-07-05 17:09:19 +0900

    Test/14  (#44)
    * :test_tube: 쿠폰 발급 동시성 테스트(CyclicBarrier 로 구현)
    
    * :test_tube: 쿠폰 발급 동시성 테스트(CyclicBarrier + Coroutine 병합)

--- src/test/kotlin/coupong/nbc/coupongwowdeal/domain/coupon/service/CouponServiceDBTest.kt
+++ src/test/kotlin/coupong/nbc/coupongwowdeal/domain/coupon/service/CouponServiceDBTest.kt
@@ -1,9 +1,6 @@
 package coupong.nbc.coupongwowdeal.domain.coupon.service
 
 import coupong.nbc.coupongwowdeal.domain.coupon.model.v1.Coupon
-import coupong.nbc.coupongwowdeal.domain.coupon.repository.v1.CouponQueryDslRepository
-import coupong.nbc.coupongwowdeal.domain.coupon.repository.v1.CouponRepository
-import coupong.nbc.coupongwowdeal.domain.coupon.repository.v1.CouponRepositoryImpl
 import coupong.nbc.coupongwowdeal.domain.coupon.repository.v1.coupon.CouponJpaRepository
 import coupong.nbc.coupongwowdeal.domain.coupon.repository.v1.couponuser.CouponUserJpaRepository
 import coupong.nbc.coupongwowdeal.domain.coupon.service.v1.CouponService
@@ -21,12 +18,15 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import java.time.LocalDateTime
+import java.util.Collections
+import java.util.concurrent.CyclicBarrier
+import java.util.concurrent.Executors
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -176,17 +176,54 @@ class CouponServiceDBTest @Autowired constructor(
             )
         )
     }
-}
 
-@TestConfiguration
-class CouponServiceTestConfig {
+    @Test
+    fun `100명의 유저가 50개 재고의 쿠폰을 동시에 발급 시도할 때 재고 소진 이후의 발급시도는 예외를 던진다_CyclicBarrier`() {
 
-    @Bean
-    fun couponRepository(
-        couponJpaRepository: CouponJpaRepository,
-        couponUserJpaRepository: CouponUserJpaRepository,
-        couponQueryDslRepository: CouponQueryDslRepository
-    ): CouponRepository {
-        return CouponRepositoryImpl(couponJpaRepository, couponUserJpaRepository, couponQueryDslRepository)
+        // Arrange
+        val testUserSize = 100
+        val testQuantity = 50
+        val nameSet = hashSetOf<String>()
+        while (nameSet.size < testUserSize) {
+            nameSet.add(Arbitraries.strings().ofMinLength(5).ofMaxLength(10).sample())
+        }
+
+        val barrier = CyclicBarrier(testUserSize)
+        val service = Executors.newFixedThreadPool(testUserSize)
+        val users = mutableListOf<User>()
+        nameSet.forEach {
+            users.add(userRepository.saveAndFlush(User(username = it, password = "test")))
+        }
+
+        val coupon = couponJpaRepository.save(
+            Coupon(
+                name = "test",
+                expirationAt = LocalDateTime.of(2030, 1, 1, 0, 0),
+                totalQuantity = testQuantity,
+                currentQuantity = testQuantity,
+                discountPrice = 10000
+            )
+        )
+
+        val exceptions = Collections.synchronizedList(mutableListOf<Exception>())
+
+        // Act
+        users.forEach {
+            service.execute {
+                try {
+                    barrier.await()
+                    couponService.issueCouponToUser(coupon.id!!, it.id!!)
+                } catch (e: Exception) {
+                    println(e.message)
+                    exceptions.add(e)
+                }
+            }
+        }
+
+        // Assert
+        assertEquals(testUserSize - testQuantity, exceptions.size)
+        exceptions.forEach { exception ->
+            assertTrue(exception is IllegalArgumentException)
+        }
     }
 }
