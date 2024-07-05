5fe91dad1ac3317dbc3324e7302ab8c8f700a27b
itssu <ssujinn96@gmail.com>
2024-07-05 10:42:43 +0900

    🔧 Redis Configuration 설정
    Co-authored-by: npureaun <kjihun89@gmail.com>

---
+++ src/main/kotlin/coupong/nbc/coupongwowdeal/infra/redis/RedisConfig.kt
@@ -0,0 +1,28 @@
+package coupong.nbc.coupongwowdeal.infra.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory()
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.connectionFactory = redisConnectionFactory
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        return redisTemplate
+    }
+}
