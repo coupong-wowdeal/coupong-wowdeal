466636a3ee9e71793efb695f185001df924007d9
itssu <ssujinn96@gmail.com>
2024-07-09 16:38:31 +0900

    🔧 redisson 설정
    Co-authored-by: npureaun <kjihun89@gmail.com>

--- src/main/kotlin/coupong/nbc/coupongwowdeal/infra/redis/RedisConfig.kt
+++ src/main/kotlin/coupong/nbc/coupongwowdeal/infra/redis/RedisConfig.kt
@@ -1,28 +1,38 @@
 package coupong.nbc.coupongwowdeal.infra.redis
 
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import redis.embedded.RedisServer
 
 @Configuration
 @EnableRedisRepositories
-class RedisConfig {
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") private val host: String,
+    @Value("\${spring.data.redis.port}") private val port: Int,
+) {
+    private var redisServer: RedisServer = RedisServer(port)
 
-    @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory()
+    @PostConstruct
+    fun startRedis() {
+        redisServer.start()
+    }
+
+    @PreDestroy
+    private fun stopRedis() {
+        redisServer.stop()
     }
 
     @Bean
-    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
-        val redisTemplate = RedisTemplate<String, String>()
-        redisTemplate.connectionFactory = redisConnectionFactory
-        redisTemplate.keySerializer = StringRedisSerializer()
-        redisTemplate.valueSerializer = StringRedisSerializer()
-        return redisTemplate
+    fun redissonClient(): RedissonClient {
+        val config: Config = Config()
+        config.useSingleServer().address = "redis://${host}:${port}"
+        return Redisson.create(config)
     }
 }
